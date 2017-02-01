c0c17b8878d8b6a36e414ee9110a5bee9973e185
Amaury Vidal <amaury@MacBook-Pro-de-Amaury.local>
2017-02-01 13:36:24 +0100

    Storyboard view for send controller

--- Classes/ViewController/SendController.swift
+++ Classes/ViewController/SendController.swift
@@ -1,46 +1,31 @@
 import UIKit
 
-@objc protocol SendControllerDelegate {
-    func didSendMessage(_ text: String)
+protocol SendControllerDelegate: class {
+    func didSend(message text: String)
 }
 
 class SendController: UIViewController {
-    var textView: UITextView!
+    @IBOutlet weak var textView: UITextView!
+    
     var text: String = ""
     weak var delegate: SendControllerDelegate?
-    
-    init(delegate:SendControllerDelegate?) {
-        self.delegate = delegate
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 
     deinit {
-        self.delegate = nil
+        delegate = nil
     }
     
-    override func loadView() {
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(SendController.send(_:)))
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SendController.cancel(_:)))
-        self.textView = UITextView(frame: CGRect(x: 0, y: 0, width: 320, height: 200))
-        //textView.font=[LayoutConstants detailSmallFont];
-        textView.text = text
-        self.view = UIView(frame: UIScreen.main.bounds)
-        self.view!.addSubview(textView)
+    override func viewDidLoad() {
+        super.viewDidLoad()
         textView.becomeFirstResponder()
     }
     
-    func send(_ sender: AnyObject) {
-        self.textView.resignFirstResponder()
-        // apply pending auto corrections
-        delegate?.didSendMessage(self.textView.text!)
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func send(_ sender: AnyObject) {
+        textView.resignFirstResponder()
+        delegate?.didSend(message: textView.text)
+        dismiss(animated: true, completion: nil)
     }
     
-    func cancel(_ sender: AnyObject) {
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func cancel(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
     }
 }
